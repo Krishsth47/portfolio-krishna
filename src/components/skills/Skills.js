dd869a674d4a8baae6e78c1d792a3781f0406100
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-03-28 08:20:24 +1100

    positioning of skills list

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -8,20 +8,21 @@ export const Skills = () => {
       <Container>
         <Row>
           <Col>
-            <SectionTitle title="skills" />
+            <SectionTitle title="Skills" />
             <div class="skill-list mb-5 d-flex justify-content-between fw-bold text-center flex-wrap">
               <span className="text-info">
                 <i class="fab fa-react d-block"></i>React
               </span>
-              <span className="text-primary">
-                <i class="fab fa-html5 d-block"></i> HTML5
+              <span className="text-warning">
+                <i class="fab fa-js-square d-block"></i>JAVASCRIPT
               </span>
+
               <span className="text-danger">
                 {" "}
                 <i class="fab fa-css3-alt d-block"></i>CSS3
               </span>
-              <span className="text-warning">
-                <i class="fab fa-js-square d-block"></i>JAVASCRIPT
+              <span className="text-primary">
+                <i class="fab fa-html5 d-block"></i> HTML5
               </span>
             </div>
           </Col>
