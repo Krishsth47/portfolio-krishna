4af28661f027fac27b507ecab0c90f11cc230519
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-05-01 12:48:22 +1000

    vslide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // or less ideally
 // import { Button } from "react-bootstrap";
-
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { DefaultLayout } from "./components/Layout/DefaultLayout";
@@ -11,17 +11,34 @@ import { Contact } from "./components/contact/Contact";
 import { Hero } from "./components/hero/Hero";
 
 const App = () => {
+  const [showComp, setShowState] = useState("home");
+
+  const handleOnClick = (value) => {
+    console.log(value);
+  };
+
   return (
     <div className="wrapper">
       <BrowserRouter>
         <DefaultLayout>
+          <div className="slider">
+            <i
+              class="fas fa-dot-circle"
+              onClick={() => handleOnClick("home")}
+            ></i>
+            <i
+              class="fas fa-dot-circle"
+              onClick={() => handleOnClick("skills")}
+            ></i>
+          </div>
           <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="about-me" element={<About />} />
             <Route path="skills" element={<Skills />} />
             <Route path="projects" element={<Projects />} />
             <Route path="contact" element={<Contact />} />
-            {/* <Skills />
+            {/* 
+            {showComp === "skill" && <Skills />}
             <Projects />
 
             <About />
